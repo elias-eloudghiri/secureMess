929693e71dbe057db47f2def824364ed038f3e60
elias <elias.eloudghiri@gmail.com>
2026-06-05 22:29:17 +0200

    Front : ajout ssl

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteBasicSslPlugin()],
   server: {
+    https: true,
     proxy: {
       "/api": {
         target: "http://localhost:8080",
