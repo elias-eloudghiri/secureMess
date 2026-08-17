e796f030c362d929d0817749fd23595a87560e99
elias <elias.eloudghiri@gmail.com>
2026-08-17 12:32:50 +0200

    Ajout monitoring WIP

--- backend/src/main/java/com/securemessage/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/securemessage/backend/config/SecurityConfig.java
@@ -77,7 +77,13 @@ public class SecurityConfig {
                         }))
         .authorizeHttpRequests(
             auth ->
-                auth.requestMatchers("/api/auth/**", "/ws/chat", "/ws/chat/**", "**/health", "**/prometheus", "**/info")
+                auth.requestMatchers(
+                        "/api/auth/**",
+                        "/ws/chat",
+                        "/ws/chat/**",
+                        "**/health",
+                        "**/actuator/prometheus",
+                        "**/info")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
